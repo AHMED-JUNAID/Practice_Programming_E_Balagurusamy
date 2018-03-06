7a15a34f94f03489b6aed489e4a18ee7a2afef6e
AHMED-JUNAID <37082978+AHMED-JUNAID@users.noreply.github.com>
2018-03-06 20:04:05 +0500

    Include Comments

--- Practice_E_Balagurusamy_CSharp/Practice_E_Balagurusamy_CSharp/Program.cs
+++ Practice_E_Balagurusamy_CSharp/Practice_E_Balagurusamy_CSharp/Program.cs
@@ -5,11 +5,16 @@ using System.Text;
 
 namespace Practice_E_Balagurusamy_CSharp
 {
+    /*
+     * this program uses namespace and comment lines
+     */
     class Program
     {
+        //Main method begins
         static void Main(string[] args)
         {
-            Console.WriteLine("C# is sharper than C++");
+            Console.WriteLine("Hello!");
         }
+        //Main method ends
     }
 }
